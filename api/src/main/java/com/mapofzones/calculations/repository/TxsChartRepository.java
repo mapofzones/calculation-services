12bb19ce006151c52af954d74cee66f973c57443
Maksim Hryhoryeu <wolthrax@gmail.com>
2023-02-05 23:25:50 +0300

    Update api. Get transactions count

--- api/src/main/java/com/mapofzones/calculations/repository/TxsChartRepository.java
+++ api/src/main/java/com/mapofzones/calculations/repository/TxsChartRepository.java
@@ -2,12 +2,13 @@ package com.mapofzones.calculations.repository;
 
 import com.mapofzones.calculations.repository.entities.TxsChart;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface TxsChartRepository extends MongoRepository<TxsChart, String> {
 
     @Transactional
-    //@Query("{'data.zone': {$eq: ?0}}")
-    TxsChart findByData_Zone(String zone);
+    @Query(value = "{'data.zone': '?0'}", fields = "{'data.chart': {$slice: ?1}}")
+    TxsChart findByData_Zone(String zone, Long period);
 
 }
